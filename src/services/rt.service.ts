6c7d952110d3b1baa778cc827e9d35993371b4be
purboyndra <purboindra@gmail.com>
2025-06-28 05:38:51 +0700

    refactor: enhance error handling in createRt function

--- src/services/rt.service.ts
+++ src/services/rt.service.ts
@@ -1,4 +1,6 @@
+import { Rt } from "../../generated/prisma";
 import prisma from "../../prisma/client";
+import { AppError } from "../utils/errors";
 
 interface CreateRtInput {
   name: string;
@@ -13,15 +15,23 @@ export const getAllRt = async () => {
   return await prisma.rt.findMany();
 };
 
-export const createRt = async (data: CreateRtInput) => {
-  return await prisma.rt.create({
-    data: {
-      name: data.name,
-      address: data.address,
-      totalFunds: data.totalFunds || 0,
-      users: { connect: data.users?.map((id) => ({ id })) },
-      activities: { connect: data.activities?.map((id) => ({ id })) },
-      news: { connect: data.news?.map((id) => ({ id })) },
-    },
-  });
+export const createRt = async (data: CreateRtInput): Promise<Rt> => {
+  try {
+    const rt = await prisma.rt.create({
+      data: {
+        name: data.name,
+        address: data.address,
+        totalFunds: data.totalFunds || 0,
+        users: { connect: data.users?.map((id) => ({ id })) },
+        activities: { connect: data.activities?.map((id) => ({ id })) },
+        news: { connect: data.news?.map((id) => ({ id })) },
+      },
+    });
+
+    return rt;
+  } catch (error) {
+    throw error instanceof AppError
+      ? error
+      : new AppError("Failed to create rt", 500);
+  }
 };
