59774a401d7168245b031ed84f182e33d8177fcd
purboyndra <purboindra@gmail.com>
2025-06-28 19:43:51 +0700

    refactor: streamline createRefreshToken function by using service methods

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,49 +1,34 @@
 import prisma from "../../prisma/client";
 import { AppError } from "../utils/errors";
 import { generateAccessToken, generateRefreshToken } from "../utils/jwt";
-import { findUserByWhatsAppNumber } from "./user.service";
+import { findRtById } from "./rt.service";
+import { findUserById, findUserByWhatsAppNumber } from "./user.service";
 
 export const createRefreshToken = async (userId: string) => {
   try {
-    const findUser = await prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
+    const user = await findUserById(userId);
 
-    if (!findUser) {
-      throw new AppError("User not found", 404);
-    }
-
-    const findRt = await prisma.rt.findUnique({
-      where: {
-        id: findUser.rtId,
-      },
-    });
-
-    if (!findRt) {
-      throw new AppError("Rt not found", 404);
-    }
+    const rt = await findRtById(user.rtId);
 
     const accessToken = generateAccessToken({
-      address: findUser.address,
-      id: findUser.id,
-      createdAt: findUser.createdAt,
-      name: findUser.name,
-      phone: findUser.phone,
-      role: findUser.role,
+      address: user.address,
+      id: user.id,
+      createdAt: user.createdAt,
+      name: user.name,
+      phone: user.phone,
+      role: user.role,
       rt: {
-        id: findRt.id,
-        name: findRt.name,
-        address: findRt.address,
+        id: rt.id,
+        name: rt.name,
+        address: rt.address,
         activities: [],
-        totalFunds: findRt.totalFunds,
+        totalFunds: rt.totalFunds,
         users: [],
-        createdAt: findRt.createdAt,
+        createdAt: rt.createdAt,
       },
     });
 
-    const refreshToken = generateRefreshToken(findUser.id);
+    const refreshToken = generateRefreshToken(user.id);
 
     await prisma.refreshToken.create({
       data: {
@@ -51,7 +36,7 @@ export const createRefreshToken = async (userId: string) => {
         /// Set the refresh token to expire in 7 days
         expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         revoked: false,
-        userId: findUser.id,
+        userId: user.id,
       },
     });
 
