528328b1a38aa30a81eeb3cb5f4767a7c3240f06
purboyndra <purboindra@gmail.com>
2025-07-09 05:38:36 +0700

    feat: authenticateToken middleware

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import * as authService from "../services/auth.service";
 import { AppError } from "../utils/errors";
 import { generateOtp } from "../services/telegeram.service";
@@ -65,6 +65,7 @@ export const revokeRefreshToken = async (req: Request, res: Response) => {
 };
 
 export const signIn = async (req: Request, res: Response) => {
+  console.log("signIn");
   try {
     const { phone } = req.body;
 
@@ -140,6 +141,51 @@ export const verifyOtp = async (req: Request, res: Response) => {
     const message =
       error instanceof AppError ? error.message : "Internal server error";
     console.error("Error verify otp", message);
+    res.status(statusCode).json({ message, data: null });
+    return;
+  }
+};
+
+export const authenticateToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    console.log("authenticateToken middleware called");
+
+    const authHeader = req.headers["authorization"];
+
+    console.log("authenticateToken authHeader", authHeader);
+
+    if (!authHeader) {
+      throw new AppError("Authorization header missing", 401);
+    }
+
+    const token = authHeader && authHeader.split(" ")[1];
+
+    console.log("authenticateToken token", token);
+
+    if (!token) {
+      throw new AppError("Token missing", 401);
+    }
+
+    const jwt = verifyJwt(token);
+
+    console.log("authenticateToken jwt", jwt);
+
+    if (typeof jwt !== "object") {
+      throw new AppError("Unauthorized", 401);
+    }
+
+    req.user = jwt;
+
+    next();
+  } catch (error) {
+    console.error("Error authenticateToken", error);
+    const statusCode = error instanceof AppError ? error.statusCode : 500;
+    const message =
+      error instanceof AppError ? error.message : "Internal server error";
     res.status(statusCode).json({ message, data: null });
     return;
   }
