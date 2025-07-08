38a66ce7382e165020d1015c725398b301739212
purboyndra <purboindra@gmail.com>
2025-07-09 05:38:23 +0700

    fix: users.routes.ts

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllUsers, createUser } from "../../src/controllers/user.controller";
+import { getAllUsers, createUser } from "../controllers/user.controller";
 
 const router = Router();
 
