d0d9f259b5b87d84c853fa91edc8f51d9e4ee849
purboyndra <purboindra@gmail.com>
2025-07-10 22:03:12 +0700

    fix: middleware users.routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,13 +3,15 @@ import {
   getAllUsers,
   createUser,
   deleteUser,
+  findUserByPhone,
 } from "../controllers/user.controller";
+import { authenticateToken } from "../middleware/authenticate.midldeware";
 
 const router = Router();
 
 router.get("/", getAllUsers);
-router.post("/", createUser);
-router.delete("/:phone", deleteUser);
-router.get("/:phone", deleteUser);
+router.post("/", authenticateToken, createUser);
+router.delete("/:phone", authenticateToken, deleteUser);
+router.get("/:phone", findUserByPhone);
 
 export default router;
