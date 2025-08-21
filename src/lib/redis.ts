57b5c1960d9c0b478a0e615a86c85430a222978a
purbo <purba.gumelar@intra.adr>
2025-08-21 12:23:05 +0700

    fix: use redis public url

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,7 +1,5 @@
 import { createClient } from "redis";
 
-const redis = createClient({
-  url: process.env.REDIS_URL,
-});
+const redis = createClient({ url: process.env.REDIS_PUBLIC_URL });
 
 export default redis;
