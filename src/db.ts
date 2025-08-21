b15d65b52a7076d0d8c0ba33a89f2eb9ff23790f
purboyndra <purboindra@gmail.com>
2025-08-21 19:48:52 +0700

    feat: implement prisma accelerate

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,14 @@
-import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import { withOptimize } from "@prisma/extension-optimize";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL || "",
   ssl: { rejectUnauthorized: false },
 });
 
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter }).$extends(
+  withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY || "" })
+);
 
 export default prisma;
