1d688df38a38db94c3ae0d6de1e86b508f88c51a
purboyndra <purboindra@gmail.com>
2025-08-23 21:23:07 +0700

    fix: use ts dist

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,4 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { withOptimize } from "@prisma/extension-optimize";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import dotenv from "dotenv";
 
