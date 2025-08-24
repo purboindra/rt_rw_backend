6b824155ef11bd3975c2b26614ef5943142a4805
purboyndra <purboindra@gmail.com>
2025-08-24 16:24:02 +0700

    feat: add field publicMessage AppError

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,8 +1,10 @@
 export class AppError extends Error {
   statusCode: number;
-  constructor(message: string, statusCode: number) {
+  publicMessage: string;
+  constructor(message: string, statusCode: number, publicMessage: string) {
     super(message);
     this.name = "AppError";
     this.statusCode = statusCode;
+    this.publicMessage = publicMessage;
   }
 }
