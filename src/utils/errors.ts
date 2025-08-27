8c6d9731c0dad5dfd5885c4d11e87cff16a0fd99
purboyndra <purboindra@gmail.com>
2025-08-28 05:41:47 +0700

    fix: set publicMessage property as nullable

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,7 +1,7 @@
 export class AppError extends Error {
   statusCode: number;
-  publicMessage: string;
-  constructor(message: string, statusCode: number, publicMessage: string) {
+  publicMessage?: string;
+  constructor(message: string, statusCode: number, publicMessage?: string) {
     super(message);
     this.name = "AppError";
     this.statusCode = statusCode;
