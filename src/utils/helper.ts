ba0d5fff1a3a25040425e6fd4b79c6c77335d0e7
purboyndra <purboindra@gmail.com>
2025-09-07 05:47:18 +0700

    feat: pruneUndefined helper

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,3 +3,11 @@ export const getTelegramKeyForRedis = (arg: string) => {
   const key = `tg:verify:${token}`;
   return key;
 };
+
+export function pruneUndefined<T extends Record<string, any>>(
+  obj: T
+): Partial<T> {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([, v]) => v !== undefined)
+  ) as Partial<T>;
+}
