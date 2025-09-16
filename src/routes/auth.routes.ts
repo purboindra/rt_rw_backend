2ad278485232d3ffe112ab023e147c82849eec9b
purboindra <purboindra@gmail.com>
2025-09-17 06:46:26 +0700

    feat: implement loginlimiter

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,17 +1,26 @@
+import { Router } from "express";
+import rateLimit from "express-rate-limit";
 import {
   createRefreshToken,
   revokeRefreshToken,
   signIn,
   verifyOtp,
 } from "../controllers/auth.controller";
-import { Router } from "express";
-import { authenticateToken } from "../middleware/authenticate.midldeware";
+
+const loginLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 5,
+  message: {
+    status: "error",
+    message: "Too many failed login attempts. Try again later.",
+  },
+});
 
 const router = Router();
 
 router.post("/refresh-token", createRefreshToken);
 router.post("/revoke-token", revokeRefreshToken);
-router.post("/sign-in", signIn);
+router.post("/sign-in", loginLimiter, signIn);
 router.post("/otp/verify", verifyOtp);
 
 export default router;
