36044e644155faea77319be953e5f13d7481899f
purboindra <purboindra@gmail.com>
2025-10-08 06:41:38 +0700

    fix: add validation for update and delete activity routes

--- src/routes/activities.routes.ts
+++ src/routes/activities.routes.ts
@@ -9,6 +9,7 @@ import {
   updateActivity,
 } from "../controllers/activities.controller";
 import { createActivitySchema } from "../schemas/activity.schemas";
+import { idParams } from "../schemas/general.schema";
 import { validate } from "../schemas/validate";
 
 const router = Router();
@@ -20,11 +21,35 @@ router.post(
   }),
   createActivity,
 );
-router.put("/:id", updateActivity);
-router.delete("/:id", deleteActivity);
+router.put(
+  "/:id",
+  validate({
+    params: idParams,
+  }),
+  updateActivity,
+);
+router.delete(
+  "/:id",
+  validate({
+    params: idParams,
+  }),
+  deleteActivity,
+);
 router.get("/", getAllActivities);
-router.get("/:id", getActivityById);
-router.get("/:id/users", getUsersActivity);
+router.get(
+  "/:id",
+  validate({
+    params: idParams,
+  }),
+  getActivityById,
+);
+router.get(
+  "/:id/users",
+  validate({
+    params: idParams,
+  }),
+  getUsersActivity,
+);
 router.post("/join", joinActivity);
 
 export default router;
