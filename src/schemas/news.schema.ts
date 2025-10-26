111c51313771c49f51497c47e552c2e9108e3d16
purboindra <purboindra@gmail.com>
2025-10-27 06:29:48 +0700

    fix: updateNewsSchema field with optional

--- src/schemas/news.schema.ts
+++ src/schemas/news.schema.ts
@@ -20,19 +20,9 @@ export type CreateNewsInput = z.infer<typeof createNewsSchema> & {
 };
 
 export const updateNewsSchema = z.object({
-  title: z
-    .string({
-      error: "Judul berita diperlukan",
-    })
-    .min(1, "Judul berita diperlukan")
-    .optional(),
+  title: z.string().optional(),
   description: z.string().optional(),
-  body: z
-    .string({
-      error: "Isi berita diperlukan",
-    })
-    .min(1, "Isi berita diperlukan")
-    .optional(),
+  body: z.string().optional(),
 });
 
 export type UpdateNewsInput = z.infer<typeof updateNewsSchema>;
