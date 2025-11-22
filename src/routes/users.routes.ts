f65de51246f2655b90c2860e4bcb84e758af8471
purboindra <purboindra@gmail.com>
2025-11-23 06:20:02 +0700

    feat: verify email endpoint

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -5,8 +5,9 @@ import {
   findUserByPhone,
   getAllUsers,
   requestEmailVerification,
+  verifyEmail,
 } from "../controllers/users.controller";
-import { requestEmailVerificationSchema, userSchema } from "../schemas/user.schemas";
+import { requestEmailVerificationSchema, userSchema, verifyEmailSchema } from "../schemas/user.schemas";
 import { validate } from "../schemas/validate";
 
 const router = Router();
@@ -29,12 +30,12 @@ router.post(
   requestEmailVerification,
 );
 
-// router.post(
-//   "/verify-email",
-//   validate({
-//     body: verifyEmailSchema,
-//   }),
-//   verifyEmail,
-// );
+router.post(
+  "/verify-email",
+  validate({
+    body: verifyEmailSchema,
+  }),
+  verifyEmail,
+);
 
 export default router;
