7882a9b7b3a0b88dbb647f71a9b92d483ddfbab8
purboindra <purboindra@gmail.com>
2025-11-23 06:19:52 +0700

    feat: add logger error redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,5 +1,8 @@
 import { createClient } from "redis";
+import { logger } from "../logger";
 
 const redis = createClient({ url: process.env.REDIS_PUBLIC_URL });
+
+redis.on("error", (err) => logger.error({ err }, "Redis Client Error"));
 
 export default redis;
