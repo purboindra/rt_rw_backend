990515d60305d4739928ff76a43cefc8ed03ca84
purboindra <purboindra@gmail.com>
2025-11-24 03:29:17 +0700

    fix: connection redis with REDIS_URL variable

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 import { logger } from "../logger";
 
-const redis = createClient({ url: process.env.REDIS_PUBLIC_URL });
+const redis = createClient({ url: process.env.REDIS_URL });
 
 redis.on("error", (err) => logger.error({ err }, "Redis Client Error"));
 
